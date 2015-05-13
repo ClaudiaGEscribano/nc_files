b4f73d1c3362431a109858a23bf0a4d6e5c1de94
ClaudiaGEscribano <claudia.g.escribano@gmail.com>
2015-05-13 16:33:47 +0200

    Variabilidad de radiacion 1989-2008 modelo

---
+++ rsds.R
@@ -0,0 +1,115 @@
+## This script describes rsds within the IP for the 1989-2008 period.
+
+## libraries that I need
+
+library(raster)
+library(rasterVis)
+library(maps)
+library(maptools)
+library(mapdata)
+library(rgdal)
+library(zoo)
+library(ncdf4)
+
+## The nc files are merge in time with cdo tools.
+
+## Variabilidad anual (o interanual). Variabilidad de la irradiacion anual
+
+## cdo mergetime *.nc rsds89_08.nc
+## cdo yearmean rsds89_08.nc rsdsym.nc
+## cdo mulc,8760 rsdsym.nc SISym.nc
+## cdo divc,1000 SISym.nc SISymk.nc
+## cdo timstd SISymk.nc std_anual.nc
+
+SISymk <- stack("SISymk.nc", varname="rsds")
+idx <- seq(as.Date('1989-07-02'), as.Date('2008-07-02'), 'year')
+SISym <- setZ(SISymk, idx)
+names(SISym) <- idx
+
+## mask fot the IP
+
+ext <- as.vector(extent(SISymk))
+boundaries <- map('worldHires', region=c('Spain','Portugal'),fill=TRUE, xlim=ext[1:2], ylim= ext[3:4], plot=FALSE)
+boundaries$names
+IDs <- sapply(strsplit(boundaries$names, ":"), function(x) x[1])
+boundaries_sp<- map2SpatialPolygons(boundaries, IDs=IDs, proj4string=CRS(projection(SISym)))
+
+IP <- mask(SISym, boundaries_sp)
+IP <- setZ(IP, idx)
+
+stdAnual <- stack("std_anual.nc", varname="rsds")
+variabilidadAnual <- stdAnual/mean(IP)
+levelplot(variabilidadAnual, contour=TRUE)
+
+histogram(variabilidadAnual)
+boxplot(variabilidadAunal)
+
+## Variabilidad mensual
+
+## cdo ymonmean rsds89_08.nc rsdsmm89_08.nc ##media de eneros, feb
+## cdo muldpm rsdsmm89_08.nc SISmm89_08.nc
+## cdo mulc,24 SISmm89_08.nc SISmm.nc
+## cdo divc,1000 SISmm.nc SISmmk.nc
+## cdo monmean rsds89_08.nc rsdsmm.nc
+## cdo muldpm rsdsmm.nc SISm.nc
+## cdo mulc,24 SISm.nc SIS.nc
+## cdo divc,1000 SIS.nc SISkwm.nc
+## cdo timselstd,20[,[12]] SISkwm.nc std_mensual.nc
+
+SISmm <- stack("SISmmk.nc", varname="rsds")
+idx <- seq(as.Date('2008-01-01'), as.Date('2008-12-02'), 'month')
+SISmm <- setZ(SISmm, idx)
+names(SISmm) <- idx
+
+IPmm <- mask(SISmm, boundaries_sp)
+IPmm <- setZ(IPmm, idx)
+
+stdMensual <- stack("std_mensual.nc", varname="rsds")
+variabilidadMensual <- stdMensual/mean(IPmm)
+levelplot(variabilidadMensual, contour=TRUE)
+names(variabilidadMensual) <- idx
+
+histogram(variabilidadMensual)
+boxplot(variabilidadMensual)
+
+## Variabilidad de la radiacion global diaria
+
+## cdo mulc,24 rsds89_08.nc SISd.nc
+## cdo divc,1000 SISd.nc SISdkw.nc
+
+SISd <- stack("SISdkw.nc", varname="rsds")
+idx <- seq(as.Date('1989-01-01'), as.Date('2008-12-31'), 'day')
+SISd <- setZ(SISd, idx)
+names(SISd) <- idx
+
+IPd <- mask(SISd, boundaries_sp)
+IPd <- setZ(IPd, idx)
+
+days <- function(x) as.numeric(format(x, '%d'))
+days(as.Date('1989-01-01'))
+SISd <- zApply(SISd, by=days, fun='mean') # tarda demasiado
+
+## cdo ydaymean SISdkw.nc SISday.nc
+
+SISd <- stack('SISday.nc', varname='rsds')
+idx <- seq(1:366)
+SISd <- setZ(SISd, idx)
+names(SISd) <- idx
+
+IPd <- mask(SISd, boundaries_sp)
+IPd <- setZ(IPd, idx)
+
+## cdo timselstd,20[,[365]] SISdkw.nc std_diaria.nc
+
+stdDiaria <- stack ("std_diaria.nc", varname="rsds")
+variabilidadDiaria <- stdDiaria/mean(IPd)
+
+## Se puede calcular la variabilidad de la radiacion en toda la peninsula para comparar con los valores de cada sitio.
+
+mean <- cellStats(SISym, stat='mean')
+sd <- cellStats(SISym, stat='sd')
+cvIP <- sd/mean
+summary(cvIP)
+
+                  
+^
